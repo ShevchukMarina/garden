305a9a944b1927b9ea6073d5f492c038a7b0fb3a
Marina Shevchuk <shev.marinet@gmail.com>
2020-10-21 00:00:49 +0300

    Removed proxy

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import  ReduxToastr from 'react-redux-toastr'
+import ReduxToastr from 'react-redux-toastr'
 import store from './store'
 
 ReactDOM.render(
